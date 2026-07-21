8f23c18171e74e5cf272e011359c9cc3cd427eba
Aline <alinemarquini@gmail.com>
2026-07-20 22:31:43 -0300

    Limpeza e análise das colunas Volume_Cirurgia_Mes e Lentes_Premium_Pct

--- 001-script.py
+++ 001-script.py
@@ -32,3 +32,56 @@ df_final = df_final.drop(columns=['Nome_Normalizado'])
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 print(df_final.head())
+
+#Temos alguns estados escritos em letras minúsculas, vamos padronizá-los
+df['Regiao'] = df['Regiao'].str.strip().str.upper()
+print("Siglas de estados padronizadas:")
+print(df['Regiao'].unique())
+
+#Analisando a coluna de Volume de cirurgias por mês, é possível encontrar alguns valores faltantes (nan)
+print('Quantidade de valores nulos:', df['Volume_Cirurgias_Mes'].isnull().sum())
+
+# Além disso, encontram-se valores do tipo float, porém, é mais interessante um número do tipo inteiro para esta análise
+df['Volume_Cirurgias_Mes'].describe 
+print('Média de cirurgias por mês', df['Volume_Cirurgias_Mes'].mean().astype(int))
+
+# Para contornar o problemas de dados faltantes, vamos nos utilizar da mediana para evitar possíveis vieses que a média poreia nos trazer, caso existam outliers no banco de dados
+mediana_volume = df['Volume_Cirurgias_Mes'].median()
+df['Volume_Cirurgias_Mes'] = df['Volume_Cirurgias_Mes'].fillna(mediana_volume).astype(int)
+print('Nulos restantes:',df['Volume_Cirurgias_Mes'].isnull().sum())
+
+# Vamos passar para linha de lentes premium vendidas
+print(df['Lentes_Premium_Pct'].head(10))
+df['Lentes_Premium_Pct'].describe
+
+#Aqui, temos os dados como objeto e queremos que seja do tipo float. Primeiro, padronizamos a coluna e, depois, trocamos o tipo da string
+df['Lentes_Premium_Pct'] = (
+    df['Lentes_Premium_Pct']
+    .astype(str)
+    .str.replace('%', '', regex=False)
+    .str.replace(',', '.', regex=False)
+    .str.strip()
+)
+df['Lentes_Premium_Pct'] = pd.to_numeric(df['Lentes_Premium_Pct'], errors='coerce')
+print(df['Lentes_Premium_Pct'].describe())
+print('Quantidade de valores nulos:', df['Lentes_Premium_Pct'].isnull().sum())
+
+#Assim como no caso do volume de cirurgias, a mediana também é o mais indicado nesta coluna para evitar viés de qualquer outlier
+mediana_lentes = df['Lentes_Premium_Pct'].median()
+df['Lentes_Premium_Pct'] = df['Lentes_Premium_Pct'].fillna(mediana_lentes)
+
+print('Nulos restantes:',df['Lentes_Premium_Pct'].isnull().sum())
+
+#Avaliando a coluna Equipamento_Laser
+df['Equipamento_Laser'].describe
+print(df['Equipamento_Laser'].unique())
+
+# Foi possível notar missing values mascarados
+
+# 2. Definindo o nome/caminho do arquivo de saída
+nome_arquivo = 'meu_arquivo.csv'
+
+# 3. Exportando o DataFrame para CSV
+df.to_csv(nome_arquivo, index=False, encoding='utf-8-sig', sep=';')
+
+print(f"Arquivo '{nome_arquivo}' gerado com sucesso!")
